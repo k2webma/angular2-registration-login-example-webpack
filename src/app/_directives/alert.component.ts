f06bf03996ea21c61b1f0ed07137724be0c1aed0
k2comma <k2comma@dongbuinc.com>
2017-05-12 11:10:13 +0900

    Let's follow 'Angular2 User Registration and Login Example'(by Auth Guard)

--- src/app/_directives/alert.component.ts
+++ src/app/_directives/alert.component.ts
@@ -2,9 +2,15 @@
 
 import { AlertService } from '../_services/index';
 
+/**
+ * Alert Component
+ *
+ * This alert component passes alert messages to the template whenever a message is received from the alert service.
+ * It does this by subscribing to the alert services's getMessage() method which returns an Observable.
+ */
 @Component({
     moduleId: module.id.toString(),
-    selector: 'alert',
+    selector: 'alert', /* define the name of this component */
     templateUrl: 'alert.component.html'
 })
 
--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,14 +1,19 @@
 ﻿import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
+/**
+ * This auth guard is used to prevent unauthenticated users from accessing restricted routes,
+ * in this example it's used in app.routing.ts to protect the home page route.
+ */
 @Injectable()
 export class AuthGuard implements CanActivate {
 
     constructor(private router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
         if (localStorage.getItem('currentUser')) {
-            // logged in so return true
+            // logged in so return ture
             return true;
         }
 
